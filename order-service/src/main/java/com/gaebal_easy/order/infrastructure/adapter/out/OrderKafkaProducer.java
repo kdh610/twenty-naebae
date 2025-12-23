9dfa5c8c6469163a1699ba29105380d54bc62571
kdh610 <k_dh610@naver.com>
2025-12-23 23:22:01 +0900

    refactor: 외부와의 연결 객체 infrastructure로 이동

--- order-service/src/main/java/com/gaebal_easy/order/infrastructure/adapter/out/OrderKafkaProducer.java
+++ order-service/src/main/java/com/gaebal_easy/order/infrastructure/adapter/out/OrderKafkaProducer.java
@@ -1,6 +1,8 @@
 package com.gaebal_easy.order.infrastructure.adapter.out;
 
 import com.gaebal_easy.order.application.dto.CreateOrderKafkaDto;
+import com.gaebal_easy.order.domain.entity.Order;
+import com.gaebal_easy.order.presentation.dto.CreateOrderRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,16 +11,25 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class CreateOrderKafkaProducer {
+public class OrderKafkaProducer implements OrderProducer{
 
     private final KafkaTemplate kafkaTemplate;
 
     /**
      * Order -> Store 배송생성 요청
-     * @param orderCreateKafkaDto
      */
-    public void createDelivery(CreateOrderKafkaDto orderCreateKafkaDto){
-        kafkaTemplate.send("order_create","order", orderCreateKafkaDto);
+    @Override
+    public void sendMessageToStore(CreateOrderRequest dto, Order order) {
+        kafkaTemplate.send("order_create", "order", CreateOrderKafkaDto.builder()
+                .orderId(order.getId())
+                .supplierId(dto.getSupplierId())
+                .receiverId(dto.getReceiverId())
+                .products(dto.getProducts())
+                .build());
     }
 
+    @Override
+    public void sendMessageToHubDecreaseRealStock(CreateOrderRequest dto) {
+        kafkaTemplate.send("confirm_stock", "product_list", dto.getProducts());
+    }
 }

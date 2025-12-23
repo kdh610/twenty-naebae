00c3176329e2ffbb2435ad3301e685de790fdd63
kdh610 <k_dh610@naver.com>
2025-12-23 23:25:23 +0900

    chore: 불필요한 import delete

--- order-service/src/main/java/com/gaebal_easy/order/application/dto/CheckStockDto.java
+++ order-service/src/main/java/com/gaebal_easy/order/application/dto/CheckStockDto.java
@@ -1,6 +1,5 @@
 package com.gaebal_easy.order.application.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.gaebal_easy.order.presentation.dto.ProductRequestDto;
 import lombok.*;
 
@@ -16,5 +15,5 @@ public class CheckStockDto {
 
     private List<ProductRequestDto> products;
     private UUID hubId;
-//    private UUID orderId;
+
 }
--- order-service/src/main/java/com/gaebal_easy/order/application/dto/UpdateOrderDto.java
+++ order-service/src/main/java/com/gaebal_easy/order/application/dto/UpdateOrderDto.java
@@ -1,6 +1,5 @@
 package com.gaebal_easy.order.application.dto;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
